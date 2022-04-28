56811d57d0170a1337cd784a4ab0e0d5f61f8fee
Zachary Farmer <zfarmer313@gmail.com>
2022-04-28 16:10:57 -0500

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,177 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Zachary Farmer on 9/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // Outlet for the equation being worked.
+    @IBOutlet weak var calculatorWorkings: UILabel!
+    //Outlet for equation results.
+    @IBOutlet weak var calculatorResults: UILabel!
+    
+    //Equation starts out as a blank string
+    var workings:String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        clearAll()
+        // Do any additional setup after loading the view.
+    }
+    //Function fof clear button, clears equation and results.
+    func clearAll() {
+        workings = ""
+        calculatorResults.text = ""
+        calculatorWorkings.text = ""
+    }
+    
+    //Function to update workings value with new input.
+    func addToWorkings(value: String) {
+        workings = workings + value
+        calculatorWorkings.text = workings
+    }
+    
+    
+
+    @IBAction func clearTap(_ sender: Any) {
+        clearAll()
+    }
+    //Removes last digit from equation on tap.
+    @IBAction func backTap(_ sender: Any) {
+        if (!workings.isEmpty) {
+            workings.removeLast()
+            calculatorWorkings.text = workings
+        }
+    }
+    
+    //Connections
+    @IBAction func percentTap(_ sender: Any) {
+        addToWorkings(value: "%")
+    }
+    @IBAction func divideTap(_ sender: Any) {
+        addToWorkings(value: "/")
+    }
+    @IBAction func multiplyTap(_ sender: Any) {
+        addToWorkings(value: "*")
+    }
+    @IBAction func minusTap(_ sender: Any) {
+        addToWorkings(value: "-")
+    }
+    @IBAction func plusTap(_ sender: Any) {
+        addToWorkings(value: "+")
+    }
+    @IBAction func decibleTap(_ sender: Any) {
+        addToWorkings(value: ".")
+    }
+    @IBAction func equalsTap(_ sender: Any) {
+        
+        if (validInput()) {
+            let checkWorkingsForPercent = workings.replacingOccurrences(of: "%", with: "*0.01")
+            let expression = NSExpression(format: checkWorkingsForPercent)
+            let result = expression.expressionValue(with: nil, context: nil) as! Double
+            let resultString = formatResult(result: result)
+            calculatorResults.text = resultString
+        } else {
+            let alert = UIAlertController(
+                title: "Invalid Input",
+                message: "Calculator unable to do math based on input",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func validInput() -> Bool {
+        
+        var count = 0
+        var funcCharIndexes = [Int]()
+        
+        for char in workings {
+            
+            if(specialCharacter(char: char)) {
+                funcCharIndexes.append(count)
+            }
+            count += 1
+        }
+        
+        var previous: Int = -1
+        
+        for index in funcCharIndexes {
+            if (index == 0) {
+                return false
+            }
+            if (index == workings.count - 1) {
+                return false
+            }
+            if (previous != -1) {
+                if (index - previous == 1) {
+                    return false
+                }
+            }
+            previous = index
+        }
+        return true
+    }
+    
+    func specialCharacter(char: Character) -> Bool {
+        
+        if (char == "*") {
+            return true
+        }
+        if (char == "/") {
+            return true
+        }
+        if (char == "+") {
+            return true
+        }
+        return false
+    }
+    
+    
+    func formatResult(result: Double) -> String {
+        if result.truncatingRemainder(dividingBy: 1) == 0 {
+            return String(format: "%.0f", result)
+        } else {
+            return String(format: "%.2f", result)
+        }
+    }
+    //Number connections
+    @IBAction func sevenTap(_ sender: Any) {
+        addToWorkings(value: "7")
+    }
+    @IBAction func eightTap(_ sender: Any) {
+        addToWorkings(value: "8")
+    }
+    @IBAction func nineTap(_ sender: Any) {
+        addToWorkings(value: "9")
+    }
+    @IBAction func fourTap(_ sender: Any) {
+        addToWorkings(value: "4")
+    }
+    @IBAction func fiveTap(_ sender: Any) {
+        addToWorkings(value: "5")
+    }
+    @IBAction func sixTap(_ sender: Any) {
+        addToWorkings(value: "6")
+    }
+    @IBAction func oneTap(_ sender: Any) {
+        addToWorkings(value: "1")
+    }
+    @IBAction func twoTap(_ sender: Any) {
+        addToWorkings(value: "2")
+    }
+    @IBAction func threeTap(_ sender: Any) {
+        addToWorkings(value: "3")
+    }
+    @IBAction func zeroTap(_ sender: Any) {
+        addToWorkings(value: "0")
+    }
+    
+    
+    
+}
+
